edd40d62b3e366c1461e746349cd03deaeeaa049
Justin <itzyoboii13@gmail.com>
2025-02-03 19:17:52 +0800

    hluu

---
+++ second.py
@@ -0,0 +1,17 @@
+print("Name: Justin Fernando")
+print("Student No: 202420033")
+print("Year: 2 - BSIT | transferee")
+
+firstNumber = 0
+secondNumber = 0
+
+
+firstNumber = int(input("Enter first number: "))
+secondNumber = int(input("Enter second number: "))
+
+Answer = firstNumber + secondNumber
+
+print("The sum is: " , (Answer))
+
+
+
